60ef098bb863cacfc05438c9ad6fbce3f6d47112
Jimoh Babatunde <jimoh.abdullahi@konga.com>
2020-06-01 23:13:53 +0100

    Updated

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -48,14 +48,14 @@ export default class HeaderLayout extends React.Component<Props, State> {
     render(){
             return(
                 <div className="header_container ">
-                    <div className="logo">Logo</div>
+                    <div className="logo">JT</div>
                         <ul className={ this.state.isOpen ? 'nav_links_mobile' : 'nav_links' }>
                             <li><NavLink to="/" className="link"  > Home </NavLink></li>
                             <li><NavLink to="/services" className="link " activeClassName="selectedLink" > Services </NavLink></li>
                             <li><NavLink to="/blog" className="link" activeClassName="selectedLink"> Blog </NavLink></li>
                             <li><NavLink to="/projects" className="link" activeClassName="selectedLink"> Portfolio </NavLink></li>
                             <li><NavLink to="/about" className="link" activeClassName="selectedLink" > About </NavLink></li> 
-                            <li><NavLink to="/contact" className='link' activeClassName="selectedLink"> Contact </NavLink></li>                   
+                            {/* <li><NavLink to="/contact" className='link' activeClassName="selectedLink"> Contact </NavLink></li>                    */}
                         </ul>
                     
                         
