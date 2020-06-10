d545ea3549d621ea58b0295d1ffa21b1b47c283b
Jimoh Babatunde <jimoh.abdullahi@konga.com>
2020-06-10 01:33:48 +0100

    Updated about page

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -5,10 +5,31 @@ import '../../assets/scss/about.scss';
 class About extends React.PureComponent {
 	render() {
 		return (
-			 <div className="about_container">
-				<h1>Aboute Me</h1> 
-				<p></p>
-			 </div>
+			<div className="about_container">
+				<h1 className="about_header">About me</h1>
+				<div className="content">
+
+					<div>
+						<h3>My Tech Bacground</h3>
+						<p>SMS text messaging is just as useful for businesses as it is for individuals, as it offers
+							the advantage of sending the recipient <br />specific information that would otherwise
+						</p>
+					</div>
+					<div>
+						<h3>Some few fun facts about m</h3>
+						<p>SMS text messaging is just as useful for businesses as it is for individuals, as it offers
+							the advantage of sending the recipient <br />specific information that would otherwise
+						</p>
+					</div>
+					<div>
+						<h3>Some few fun facts about m</h3>
+						<p>SMS text messaging is just as useful for businesses as it is for individuals, as it offers
+							the advantage of sending the recipient <br />specific information that would otherwise
+						</p>
+					</div>
+
+				</div>
+			</div>
 		);
 	}
 }
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../assets/scss/home.scss';
-import myPic from '../../assets/images/me.png';
+import me from '../../assets/images/me-min.png';
+
 
 export default class Home extends React.Component {
  
@@ -9,10 +10,10 @@ export default class Home extends React.Component {
         return (
             <div className="home_container">
                 <div className="content">
-                    <img src={myPic} alt="MyPicture" width="300px" height="300px"></img>
+                    <img src={me} alt="MyPicture" width="300px" height="300px"></img>
                     <h1>Jimoh Babatunde Olalekan</h1>
                     <h3>Mobile App Developer ( Swift, Objective-C, Kolin, Flutter )</h3>
-                    <p>Please click <span><a href="mailto:abdullahijimoh3.ja@gmail.com" className="styled-link">here</a></span> to have access to my CV</p>
+                    <p>Please click <span><a href="mailto:abdullahijimoh3.ja@gmail.com" className="styled-link">here</a></span> to have access to my resume</p>
                     <p>Get in touch with me via <span><a href="mailto:abdullahijimoh3.ja@gmail.com" className="styled-link">abdullahijimoh3.ja@gmail.com</a></span></p>
                 </div>
                 
